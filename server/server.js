c6db3d8312ee8a8e938f20e4a84e9cad38419fb4
Elijah Miernicki <miernickielijah@gmail.com>
2021-09-21 20:36:42 -0500

    commented out server get request until herkou deoployment

--- server/server.js
+++ server/server.js
@@ -22,9 +22,10 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+//COMMENT OUT UNTIL HEROKU DEPLOYEMENT//
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
     app.listen(PORT, () => {
