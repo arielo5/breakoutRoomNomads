16bfe6ca7d39380ca0607383d8aeb5d54c842f47
Hope <johnhope354@gmail.com>
2021-09-21 21:06:25 -0500

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
+import "./App.css";
 import React from "react";
+import Newdle from './components/Nav'
+import Login from "./components/Login"
 
 function App() {
   return (
-    <div>Newdle</div>
+    <div><Newdle />
+    <Login /></div>
+    
   );
 }
 
---
+++ client/src/components/Login.js
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div className="loginContainer">
+      <div className="login">Signup</div>
+      <div className="login">Login</div>
+    </div>
+  );
+}
