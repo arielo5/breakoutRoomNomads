e0ee70cc817036e4c570709dab44816086268dfe
Claholla <claholla@gmail.com>
2021-09-25 11:27:57 -0500

    merging conflicts

--- client/src/components/title/title.js
+++ client/src/components/title/title.js
@@ -10,7 +10,9 @@ export default function Title() {
             What should we have for dinner tonight?
           </p>
           <p className="subtitle titlebg titletext">
-            <i className="subtitle-text">By: Karl Baker<br /><span className="createdate">Created: 09/17/21 - 10:38AM</span></i>
+            <i className="subtitle-text">By: Karl Baker</i>
+            <br />
+            <i className="createdate">Created: 09/17/21 - 10:38AM</i>
           </p>
         </div>
         <div className="column is-3"></div>
