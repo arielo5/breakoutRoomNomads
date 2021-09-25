5ca38563d3c135fe33c3aa65132adf9186ceb4b9
Hope <johnhope354@gmail.com>
2021-09-25 13:05:42 -0500

    added LoggedInHamburger

---
+++ client/src/components/navbar/LoggedInHamburger.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function LoggedInHamburger() {
+  return (
+    <nav role="navigation">
+      <div id="menuToggle">
+        <input type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul id="menu">
+          <NavLink to='/'>
+            <li>Logout</li>
+          </NavLink>
+          <NavLink to=''>
+            <li>About</li>
+          </NavLink>
+          <NavLink to=''>
+            <li>Contact</li>
+          </NavLink>
+        </ul>
+      </div>
+    </nav>
+  );
+}
