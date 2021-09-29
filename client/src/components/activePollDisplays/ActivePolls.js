f830d66ead34c2b24700c8810b00323b1a61ebfe
Hope <johnhope354@gmail.com>
2021-09-28 21:08:07 -0500

    added useState to Poll Display

--- client/src/components/activePollDisplays/ActivePolls.js
+++ client/src/components/activePollDisplays/ActivePolls.js
@@ -1,1 +1,20 @@
 import React from 'react'
+
+export default function ActivePolls () {
+    return(
+        <div className="card custom-card">
+        <div className="card-content">
+          <p className="title">
+            “There are two hard things in computer science: cache invalidation,
+            naming things, and off-by-one errors.”
+          </p>
+          <p className="subtitle">Jeff Atwood</p>
+        </div>
+        <footer className="card-footer">
+          <p className="card-footer-item">
+            <span>Vote!</span>
+          </p>
+        </footer>
+      </div>
+    )
+}
--- client/src/components/pollDisplays/PollDisplay.js
+++ client/src/components/pollDisplays/PollDisplay.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState } from "react";
+import ActivePolls from '../activePollDisplays/ActivePolls'
 export default function PollDisplay() {
 
   const [currentPolls, setCurrentPolls] = useState([]);
@@ -11,20 +11,10 @@ export default function PollDisplay() {
 
   return (
     <div>
-      <div className="card custom-card">
-        <div className="card-content">
-          <p className="title">
-            “There are two hard things in computer science: cache invalidation,
-            naming things, and off-by-one errors.”
-          </p>
-          <p className="subtitle">Jeff Atwood</p>
-        </div>
-        <footer className="card-footer">
-          <p className="card-footer-item">
-            <span>Vote!</span>
-          </p>
-        </footer>
-      </div>
+      <ActivePolls
+      activePolls={currentPolls}
+      >
+      </ActivePolls>
     </div>
   );
 }
