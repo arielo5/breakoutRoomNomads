40fc0c80b3b7b53020d34af5b816f37df5edf78f
Elijah Miernicki <miernickielijah@gmail.com>
2021-09-30 17:00:40 -0500

    hompage logo centered and mobile responsive

--- client/src/App.js
+++ client/src/App.js
@@ -82,7 +82,7 @@ function App() {
                   <Footer />
                 </Route>
                 <Route exact path="/contact">
-                  <LoggedInHamburger />
+                  <Hamburger />
                   <Contact />
                   <Footer />
                 </Route>
