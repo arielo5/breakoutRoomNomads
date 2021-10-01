80d0e654dd11845420c0ca6005adbbe3cb1439c6
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-09-30 19:31:49 -0500

    linking component to dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NewPoll from "../newPoll/newPoll"
 
 export default function Dashboard() {
+    const [showPoll, setShowPoll] = useState(false);
     return (
         <div id="buttonContainer">
-            <button id="createNewPoll"><span id='buttonText'>Create Poll</span></button>
+            <button id="createNewPoll" onClick={() => setShowPoll(!showPoll)}><span id='buttonText'>Create Poll</span></button>
+            {showPoll ? <NewPoll /> : <></>}
         </div>
     )
 }
