4dc4207f27c39198995e80ee516da1b3f31b8b06
Ariel Martinez <arielo5.gamer@gmail.com>
2021-10-02 08:24:29 -0500

    Update footer.js

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-export default function Footer() {
+function Footer() {
     return (
-        <footer className="footer">
-            <div className="content has-text-centered">
-                <p>
-                    <span className="footlogo"><strong>Newdle</strong></span> by Rhyce Erickson, Clay Holland, John Hope, Ariel Martinez, and Elijah Miernicki. The source code is licensed
-                    <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. The website content
-                    is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-                </p>
+        <footer class="">
+            <div class="section-heading ">
+            <p class="is-size-5 has-text-black">
+                <a href="https://bulma.io">
+                    <img src="https://bulma.io/images/made-with-bulma.png"
+                    alt="Made with Bulma"
+                    width="128"
+                    height="24"></img>
+                </a> by <a href="https://github.com/arielo5">Ariel Martinez</a>. The
+                source code is licensed 
+                <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>.
+            </p>
             </div>
         </footer>
     )
 }
+
+export default Footer;
