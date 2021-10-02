31e715c29298f2dfe53ae71df9da9d79e3371de7
Ariel Martinez <arielo5.gamer@gmail.com>
2021-10-02 08:30:39 -0500

    Update footer.js

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React from "react";
 
-function Footer() {
-    return (
-        <footer class="">
-            <div class="section-heading ">
-            <p class="is-size-5 has-text-black">
-                <a href="https://bulma.io">
-                    <img src="https://bulma.io/images/made-with-bulma.png"
-                    alt="Made with Bulma"
-                    width="128"
-                    height="24"></img>
-                </a> by <a href="https://github.com/arielo5">Ariel Martinez</a>. The
-                source code is licensed 
-                <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>.
-            </p>
-            </div>
-        </footer>
-    )
+export default function Footer() {
+  return (
+    
+    <footer>
+    <br></br>
+    <br></br>
+    <br></br>
+      <div className="section-heading has-background-primary">
+        <p className="is-size-5 has-text-black has-text-centered">
+          <a href="https://bulma.io">
+            <img
+              src="https://bulma.io/images/made-with-bulma.png"
+              alt="Made with Bulma"
+              width="128"
+              height="24"
+            ></img>
+          </a>{" "}
+          <span className="footlogo">
+            <strong>Newdle</strong>
+          </span>{" "}
+          by Rhyce Erickson, Clay Holland, John Hope, Ariel Martinez, and Elijah
+          Miernicki.
+        </p>
+      </div>
+    </footer>
+  );
 }
-
-export default Footer;
